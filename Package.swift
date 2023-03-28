30628c30dea175786a547a490b5d58d48e05b64f
pawan-100ms <pawan@100ms.live>
2023-03-28 14:02:24 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "HMSAnalyticsSDK",
-            url: "https://github.com/100mslive/100ms-ios-analytics-sdk/releases/download/0.0.2/HMSHLSPlayerSDK.xcframework.zip",
+            url: "https://github.com/100mslive/100ms-ios-analytics-sdk/releases/download/0.0.2/HMSAnalyticsSDK.xcframework.zip",
             checksum: "40229908576cac8afab7f9ba8b3bd9b1408f97f7bff63f83dca5b4f60f4378f0"
         ),
         .binaryTarget(
