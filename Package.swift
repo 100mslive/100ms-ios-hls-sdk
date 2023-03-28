321ff36ccdbca9ed2cace9785afb6d0bad0aa002
pawan-100ms <pawan@100ms.live>
2023-03-28 13:24:06 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,18 +7,25 @@ let package = Package(
     platforms: [.iOS(.v12)],
     products: [
         .library(
+            name: "HMSAnalyticsSDK",
+            targets: ["HMSAnalyticsSDK"]),
+        .library(
             name: "HMSHLSPlayerSDK",
             targets: ["HMSHLSPlayerSDK"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/100mslive/100ms-ios-analytics-sdk", from: "0.0.2")
-    ],
     targets: [
+        .binaryTarget(
+            name: "HMSAnalyticsSDK",
+            url: "https://github.com/100mslive/100ms-ios-analytics-sdk/releases/download/0.0.2/HMSHLSPlayerSDK.xcframework.zip",
+            checksum: "40229908576cac8afab7f9ba8b3bd9b1408f97f7bff63f83dca5b4f60f4378f0"
+        ),
         .binaryTarget(
             name: "HMSHLSPlayerSDK",
             url: "https://github.com/100mslive/100ms-ios-hls-sdk/releases/download/0.0.2/HMSHLSPlayerSDK.xcframework.zip",
             checksum: "470932129c8dd358ebbe748bc1e05739f33c642779513fee17e42a117329dce2",
-            dependencies:["HMSAnalyticsSDK"]
+            dependencies: [
+                .product(name: "HMSAnalyticsSDK", package: "HMSHLSPlayerSDK"),
+            ]
         )
     ]
 )
